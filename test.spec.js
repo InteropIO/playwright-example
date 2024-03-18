c85865cb8090958e1fae3f8d1753c7f6b845750c
Tsvetan Bratanov <tsvetan.bratanov@tick42.com>
2024-03-18 15:36:29 +0200

    G4E-7314: Webgroup app Playwright example
    - Adding a webgroup test example using PlayWright and IODesktop

--- test.spec.js
+++ test.spec.js
@@ -1,55 +1,95 @@
-const { _electron: electron } = require("playwright")
-const path = require("path");
-const { test, expect } = require("@playwright/test")
+const { _electron: electron } = require('playwright');
+const { test, expect } = require('@playwright/test');
+const { setDefaultResultOrder } = require('dns');
 
-const gdDir = `${process.env.LocalAppData}\\Tick42\\GlueDesktop\\`;
-const gdExePath = path.join(gdDir, "tick42-glue-desktop.exe");
+const path = require("path");
+const IODesktop = require("@interopio/desktop");
+
+setDefaultResultOrder("ipv4first");
+const ioDesktopDir = `${process.env.LocalAppData}\\interop.io\\io.Connect Desktop\\Desktop`;
+const desktopExePath = path.join(ioDesktopDir, "io-connect-desktop.exe");
 
 test.setTimeout(60000);
 
-test("Launch Dev Tools from the Glue42 Application Manager and wait for it to appear.", async () => {
-    // Step 1: Start Glue42 Enterprise.
-    const electronApp = await electron.launch({
-        executablePath: gdExePath,
-        cwd: gdDir
-    });
+test('open two apps, snap them together and manipulate the formed group via the group frame buttons', async () => {
+  // step 1 Start IO.Desktop Enterprise
+  const electronApp = await electron.launch({
+    executablePath: desktopExePath,
+    cwd: ioDesktopDir
+  });
 
-    // Step 2: Wait for the Glue42 Application Manager to appear.
-    const { page } = await waitForAppToLoad("glue42-application-manager", electronApp);
+  // step 2 Wait for application manager to appear
+  const { page } = await waitForAppToLoad("io-connect-desktop-toolbar", electronApp);
 
-    // Step 3: Click on the element with an "apps" ID to expand the Applications view.
-    await page.click("id=apps");
+  // step 3 open two windows which will be used for testing via the desktop API
+  const desktop = await initDesktop(page);
+  const url = "https://docs.interop.io/desktop/getting-started/what-is-io-connect-desktop/general-overview/index.html";
+  const win1 = await desktop.windows.open("win1", url);
+  const win2 = await desktop.windows.open("win2", url);
 
-    // Step 4: Type "dev tools" in the app search field.
-    await page.type("id=app-search", "dev tools");
+  // step 4 snap windows to create a group
+  await win2.snap(win1.id, "right");
+  
+  // step 5 get the groupId of the windows and utilizing the helper method get the page for the webGroup
+  const groupId = win1.groupId;
+  const webGroup = await getWebGroup(groupId, electronApp);
 
-    // Step 5: Click on the result.
-    await page.click("id=search-results");
-
-    // Step 6: Wait for the Dev Tools app to appear.
-    await waitForAppToLoad("DevTools", electronApp);
+  // step 6 utilzing the page of the webgroup, maximize, restore and close the webgroup entirely
+  await webGroup.locator(`#t42-group-caption-bar-standard-buttons-maximize-${groupId}`).click();
+  await webGroup.waitForSelector(`#t42-group-caption-bar-standard-buttons-restore-${groupId}`);
+  await webGroup.locator(`#t42-group-caption-bar-standard-buttons-restore-${groupId}`).click();
+  await webGroup.locator(`#t42-group-caption-bar-standard-buttons-close-${groupId}`).click();
 });
 
-// Helper method that waits for a Glue42 app to appear.
-const waitForAppToLoad = (appName, electronApp) => {
-    return new Promise((resolve, reject) => {
-        electronApp.on("window", async (page) => {
-            try {
-                const glue42gd = await page.evaluate("glue42gd");
+// method to initialize IO desktop so it can be used within the testing environment
+const initDesktop = async (page) => {
+  // In this scenario, we are utilzing the page of the first shell application we have started to obtain a gwToken
+  const gwToken = await page.evaluate('glue42gd.getGWToken()');
+  return await IODesktop({ layouts: 'full', auth: { gatewayToken: gwToken }, activities: false });
+}
 
-                if (appName === glue42gd.application) {
-                    page.on("load", () => {
-                        resolve({
-                            app: glue42gd.application,
-                            instance: glue42gd.instance,
-                            glue42gd,
-                            page
-                        });
-                    });
-                };
-            } catch (error) {
-                // Do nothing.
-            };
-        });
+// helper method that waits for a IO.Desktop app to appear
+const waitForAppToLoad = (appName, electronApp) => {
+  return new Promise((resolve, reject) => {
+    electronApp.on('window', async (page) => {
+      try {
+        const glue42gd = await page.evaluate(`glue42gd`);
+        if (appName === glue42gd.application) {
+          page.on('load', () => {
+            resolve({
+              app: glue42gd.application,
+              instance: glue42gd.instance,
+              glue42gd,
+              page
+            });
+          })
+        }
+      } catch (e) {
+        // add proper logging
+      }
     });
+  });
+}
+
+// helper method to get the webGroup page to access it's selectors
+const getWebGroup = async (groupId, electronApp) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const windows = electronApp.windows();
+      for (let index = 0; index < windows.length; index++) {
+        const page = windows[index];
+        const glue42webGroups = await page.evaluate(
+          `if (window.glue42webGroups) {
+                  glue42webGroups
+              }`
+        );
+        if (glue42webGroups && groupId === glue42webGroups.settings.groupId) {
+          resolve(page);
+          break;
+        }
+      }
+    } catch (e) {
+      // add proper logging
+    }
+  });
 };
